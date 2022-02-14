68f4ddcf0108916d23fbf5ded003698435bdb4c7
cottonclaw <crr78@georgetown.edu>
2022-02-14 12:06:08 -0500

    updated documentation for util.py

--- util.py
+++ util.py
@@ -1,10 +1,19 @@
 # TODO: lab, homework
 def parse_sts(data_file):
+    """Reads a tab-separated sts benchmark file and returns a list of each
+       sentence pair and the corresponding similarity score
+
+    Args:
+        data_file (str): The path to the sts benchmark file
+
+    Returns:
+        tuple[list, list]: A tuple containing a list of sentence pairs and
+                           a list of similarity scores
+
+    Raises:
+        None
     """
-    Reads a tab-separated sts benchmark file and returns
-    texts: list of tuples (text1, text2)
-    labels: list of floats
-    """
+
     texts = []
     labels = []
 
