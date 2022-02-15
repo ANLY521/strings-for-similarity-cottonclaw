f463cd93f852a070d4cbbf3a033d7168863be848
cottonclaw <crr78@georgetown.edu>
2022-02-15 15:25:24 -0500

    initial nist score implementation

--- sts_pearson.py
+++ sts_pearson.py
@@ -1,6 +1,8 @@
 from scipy.stats import pearsonr
 import argparse
 from util import parse_sts
+from nltk import word_tokenize
+from nltk.translate.nist_score import sentence_nist
 
 
 def main(sts_data):
@@ -27,12 +29,48 @@ def main(sts_data):
     # HINT: Longest common substring can be complicated. Investigate difflib.SequenceMatcher for a good option.
     score_types = ["NIST", "BLEU", "Word Error Rate", "Longest common substring", "Edit Distance"]
 
-    #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
+    # NIST
+    scores = []
+    for text_pair in texts:
+        t1, t2 = text_pair
+        t1_toks = word_tokenize(t1.lower())
+        t2_toks = word_tokenize(t2.lower())
+        try:
+            nist_ab = sentence_nist([t1_toks], t2_toks)
+        except ZeroDivisionError:
+            nist_ab = 0
+        try:
+            nist_ba = sentence_nist([t2_toks], t1_toks)
+        except ZeroDivisionError:
+            nist_ba = 0
+        # assert nist_ab == nist_ba, f'Symmetrical NIST is not symmetrical! Got {nist_ab} and {nist_ba}'
+        scores.append(nist_ab)
+    score = pearsonr(scores, labels)[0]
+    print(f'NIST correlation: {score:.03f}')
+
+    # BLEU
+    # WER
+    # LCS
+    # ED
+
+    # define a function for calculating either NIST or BLEU metric
+    def nist_or_bleu_calc(text_pair, metric):
+        print('Function is not ready yet')
+
+    # define a function for calculating each of the metrics for each text pair
+    def metric_calc(metric):
+        if metric == 'NIST' or metric == 'BLEU':
+            scores = [nist_or_bleu_calc(text_pair, metric) for text_pair in texts]
+
+
+
+    # TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
     # in a table in the README
     print(f"Semantic textual similarity for {sts_data}\n")
     for metric_name in score_types:
-        score = 0.0
+        metric_scores = metric_calc(metric_name)
+        score = pearsonr(metric_scores, labels)
         print(f"{metric_name} correlation: {score:.03f}")
 
     # TODO 4: Complete writeup as specified by TODOs in README (describe metrics; show usage)
